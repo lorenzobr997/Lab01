787a78d0465cc62c85001ab90d70b7f752af7068
Lorenzo <Lorenzo@LORENZO.homenet.telecomitalia.it>
2021-03-10 11:44:25 +0100

    Manca da implementare la sezione LinkedList, ArrayList terminata

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -28,25 +28,68 @@ public class FXMLController {
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML
+    private TextArea txtPrestazioni;
+
+    @FXML
+    private Button btnCancella;
 
     @FXML
     private Button btnReset;
 
     @FXML
     void doInsert(ActionEvent event) {
+    	elenco.getElenco().clear();
+    	txtResult.setText("");
+    	String p = txtParola.getText();
+    	elenco.addParola(p);
+    	String list = ""; 
+    	
+    	for (String s : elenco.getElenco())
+    	{
+    	    list = list + s + "\n";
+    	}
+    	txtResult.appendText(list);
+    	String pres = "" + System.nanoTime();
+    	txtPrestazioni.appendText(pres + "\n");
+    	txtParola.setText("");
     	// TODO
     }
+    
+    @FXML
+    void doCancella(ActionEvent event) {
+    	String s = txtResult.getSelectedText();
+    	
+    	String list = ""; 
+    	
+    	for (String st : elenco.canc(s))
+    	{
+    	    list = list + st + "\n";
+    	}
+    	txtResult.setText("");
+    	txtResult.appendText(list);
+
+    }
+
 
     @FXML
     void doReset(ActionEvent event) {
     	// TODO
+    	elenco.reset();
+    	txtResult.setText("");
+    	txtPrestazioni.setText("");
+    	txtParola.setText("");
+    	
     }
 
     @FXML
     void initialize() {
-        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+    	assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtPrestazioni != null : "fx:id=\"txtPrestazioni\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
         elenco = new Parole() ;
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,53 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Parole {
-		
+	ArrayList<String> parole;
+	ArrayList<String> paroleo;
+	
 	public Parole() {
 		//TODO
+		parole = new ArrayList<>();
+		paroleo = new ArrayList<>();
 	}
 	
 	public void addParola(String p) {
 		//TODO
+		parole.add(p);
+	}
+	
+	public class Comp implements Comparator<String>{
+		public int compare(String p1, String p2) {
+			p1 = p1.toUpperCase();
+			p2 = p2.toUpperCase();
+			return p1.compareTo(p2);
+		}
 	}
 	
 	public List<String> getElenco() {
 		//TODO
-		return null;
+		paroleo.addAll(parole);
+		Collections.sort(paroleo, new Comp());
+		return paroleo;
 	}
 	
 	public void reset() {
 		// TODO
+		parole.clear();
+		paroleo.clear();
+	}
+	
+	public List<String> canc(String p) {
+		for(int i = 0; i < paroleo.size(); i++) {
+			if(paroleo.get(i).equals(p)) {
+				paroleo.remove(i);
+			}
+		}
+		return paroleo;
 	}
 
 }
